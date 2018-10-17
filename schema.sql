d8d78cbf9445ffd83e70f218fb8bc38e24b949da
James Wilson <jwilsoncalderdale@gmail.com>
2018-10-17 23:21:09 +0100

    change square type to enum

--- schema.sql
+++ schema.sql
@@ -6,6 +6,16 @@ CREATE SCHEMA api;
 
 CREATE TYPE COLOUR_TYPE AS ENUM ('black', 'white');
 CREATE TYPE PIECE_TYPE AS ENUM ('pawn', 'rook', 'knight', 'bishop', 'queen', 'king');
+CREATE TYPE SQUARE_TYPE AS ENUM (
+	'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 
+  'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 
+  'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 
+  'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 
+  'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 
+  'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 
+  'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 
+  'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8'
+);
 
 CREATE TABLE api.player (
 id SERIAL PRIMARY KEY,
@@ -46,7 +56,7 @@ CREATE TABLE api.piece (
 	id SERIAL PRIMARY KEY,
 	game_id INTEGER REFERENCES api.game NOT NULL,
 	turn INTEGER NOT NULL,
-	square VARCHAR(2) NOT NULL,
+	square SQUARE_TYPE NOT NULL,
 	colour COLOUR_TYPE NOT NULL,
 	type PIECE_TYPE NOT NULL
 );

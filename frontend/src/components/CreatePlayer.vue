b0bb198b28ebf01462996e39e31f87e5f10afec5
James Wilson <jwilsoncalderdale@gmail.com>
2018-10-27 22:15:40 +0100

    make basic vue app with CreatePlayer component

---
+++ frontend/src/components/CreatePlayer.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="create-player">
+    <form>
+      <label>
+        Username
+        <input type="text" v-model="username"/>
+      </label>
+
+      <label>
+        Email
+        <input type="text" v-model="email"/>
+      </label>
+
+      <label>
+        Name
+        <input type="text" v-model="name"/>
+      </label>
+
+      <label>
+        Password
+        <input type="password" v-model="password"/>
+      </label>
+
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreatePlayer",
+  props: {},
+  data() {
+    return {
+      username: "",
+      name: "",
+      email: "",
+      password: ""
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.create-player {
+  border: 1px solid black;
+  display: inline-block;
+  label {
+    display: block;
+  }
+}
+</style>

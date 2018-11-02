5f0105e1d9265e4885285d9671db05a825530d7c
James Wilson <jwilsoncalderdale@gmail.com>
2018-11-02 21:53:18 +0000

    make Login component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,18 +3,21 @@
     <h1>Simuchess</h1>
     <CreatePlayer/>
     <Stats/>
+    <Login/>
   </div>
 </template>
 
 <script>
 import CreatePlayer from './components/CreatePlayer.vue';
 import Stats from './components/Stats.vue';
+import Login from './components/Login.vue';
 
 export default {
   name: 'app',
   components: {
     CreatePlayer,
     Stats,
+    Login,
   },
 };
 </script>
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="login">
+    <h2>Login</h2>
+    <form>
+      <label>
+        Username
+        <input type="text" v-model="username"/>
+      </label>
+
+      <label>
+        Password
+        <input type="password" v-model="password"/>
+      </label>
+
+      <button v-on:click.prevent="login">
+        Login
+      </button>
+
+      <span v-if="error" class="error">Something Went Wrong</span>
+    </form>
+  </div>
+  
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Login',
+  data: () => ({
+    username: '',
+    password: '',
+    jwt: '',
+  }),
+  methods: {
+    login() {
+      return axios
+        .post('http://localhost:3000/rpc/login', {
+          given_username: this.$data.username,
+          given_password: this.$data.password,
+        })
+        .then(({ data }) => {
+          this.$data.jwt = data;
+        });
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.login {
+  border: 1px solid black;
+  display: inline-block;
+  label {
+    display: block;
+  }
+}
+</style>
